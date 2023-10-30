cf14dd58aaea0c5d6ab29a108a6c87fc5b10e25b
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-10-31 00:23:18 +1100

    Update 1st attempt

--- index.js
+++ index.js
@@ -11,29 +11,6 @@ const openai = new OpenAI({ key: process.env.OPENAI_API_KEY });
 
 
 
-const categories = ['Technology', 'Business' ]; // Add as many as you like
-
-const fetchNewsArticle = async () => {
-    try {
-        const randomCategory = categories[Math.floor(Math.random() * categories.length)];
-        const NEWS_API_URL = `https://newsapi.org/v2/top-headlines?country=us&category=${randomCategory}&apiKey=${process.env.NEWS_API}`;
-        const response = await fetch(NEWS_API_URL);
-
-        if (!response.ok) {
-            console.error(`API Error: ${response.status}`);
-            return null;
-        }
-
-        const data = await response.json();
-        return data.articles[0].title; // Gets title of first article, replace with actual attribute
-    } catch (error) {
-        console.error('Error fetching news article:', error);
-        return null;
-    }
-};
-
-
-
 
 async function downloadImage(url, path) {
     const response = await fetch(url);
@@ -62,9 +39,6 @@ async function fetchMedia(query) {
         PEXELS_API_URL = 'https://api.pexels.com/v1/search';
         responseType = 'photos';
     }
-
-
-
 
     const ITEMS_TO_FETCH = 5;
     const response = await fetch(`${PEXELS_API_URL}?query=${query}&per_page=${ITEMS_TO_FETCH}`, {
@@ -96,49 +70,67 @@ async function fetchMedia(query) {
 }
 
 
-function extractKeywords(rewrittenArticle) {
-    // Split article into array of words
-    const words = rewrittenArticle.split(/\s+/);
 
-    // Remove duplicates
-    const uniqueWords = [...new Set(words)];
+const prompts = [
 
-    // Choose up to first 5 unique words as keywords
-    const keywords = uniqueWords.slice(0, 5).join(", ");
+    "How can AI be leveraged to improve customer service in small businesses?",
+    "Explore innovative ways AI can be integrated into daily life to enhance convenience.",
+    "What are the ethical considerations when using AI for decision-making in business?",
+    "Discuss the impact of AI on job markets and the need for reskilling in the workforce.",
+    "How can startups use AI to gain a competitive edge in their industries?",
+    "Explore the role of AI in optimizing supply chain management for larger corporations.",
+    "What steps can businesses take to ensure the responsible and transparent use of AI?",
+    "Investigate the potential of AI in healthcare for early disease detection and personalized treatment.",
+    "How can AI-driven chatbots enhance e-commerce and customer support experiences?",
+    "Discuss the challenges and benefits of implementing AI-driven automation in manufacturing.",
+    "Explore AI-powered marketing strategies for small businesses with limited budgets.",
+    "How can AI be used to analyze big data and drive data-driven decisions in organizations?",
+    "Investigate the role of AI in predicting and preventing cyberattacks in the business sector.",
+    "Discuss the ethical implications of AI-generated content in media and advertising.",
+    "Explore the future of autonomous vehicles and their impact on transportation and logistics.",
+    "How can AI-driven virtual assistants improve productivity and time management in business?",
+    "Discuss the potential for AI to revolutionize the field of agriculture and increase crop yields.",
+    "Investigate the use of AI in financial services for fraud detection and risk assessment.",
+    "How can AI-powered language translation tools bridge communication gaps in global businesses?",
+    "Explore the challenges and opportunities of integrating AI into education and online learning.",
+    "Discuss the role of AI in personalizing content recommendations on streaming platforms.",
+    "How can AI-driven predictive analytics benefit small businesses in making strategic decisions?",
+    "Investigate the impact of AI on the entertainment industry, from content creation to distribution.",
+    "Explore AI-powered tools for enhancing mental health and well-being in the workplace.",
+    "Discuss the potential for AI to assist in environmental conservation and sustainability efforts.",
+    "How can AI be used to optimize energy consumption in smart homes and buildings?",
+    "Investigate the challenges of bias and fairness in AI algorithms and their implications.",
+    "How can AI and blockchain technologies be leveraged to improve customer service in small businesses?",
+    "Explore innovative ways AI and blockchain can be integrated into daily life to enhance convenience.",
+    "What are the ethical considerations when using AI and blockchain for decision-making in business?",
 
-    return keywords;
-}
 
 
 
-
-
-async function getRewrittenArticle(originalArticle) {
+];
+async function getGeneratedTweet() {
     try {
-        const prompt = `Rewrite the following news article, with a AI and Finance spin: ${originalArticle}`;
+        // Randomly pick a prompt from the prompts array
+        const randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
 
         const response = await openai.completions.create({
             model: "gpt-3.5-turbo-instruct",
-            prompt: prompt,
-            max_tokens: 300,  // Adjust based on how long you want the rewritten article to be
+            prompt: randomPrompt,
+            max_tokens: 300,
             temperature: 0.7
         });
 
         return response.choices[0].text.trim();
     } catch (error) {
-        console.log("Error rewriting article:", error);
+        console.log("Error generating tweet:", error);
     }
-
-
-
 }
-
-
-
-
 const appendHashTagsAndMentions = (tweetContent) => {
-    return `${tweetContent}  @AINewsEvents!`
+    return `${tweetContent} #AI #blockchain #technology`;
 };
+
+
+
 
 const trimToCompleteSentence = (tweet) => {
     if (tweet.length <= 280) return tweet;
@@ -150,28 +142,18 @@ const trimToCompleteSentence = (tweet) => {
     return lastValidEnd !== -1 ? tweet.substring(0, lastValidEnd + 1) : tweet.substring(0, 277) + "...";
 };
 
-
-
-
-//initiate actual tweet function
 const tweet = async () => {
     try {
-        const originalArticle = await fetchNewsArticle(); // Replace this with actual fetched article
-        const rewrittenArticle = await getRewrittenArticle(originalArticle);
-
-        if (rewrittenArticle) {
-            const fullTweet = appendHashTagsAndMentions(rewrittenArticle);  // Assuming your existing function can handle article text
+        const tweetContent = await getGeneratedTweet();
+        if (tweetContent) {
+            const fullTweet = appendHashTagsAndMentions(tweetContent);
             const trimmedTweet = trimToCompleteSentence(fullTweet);
 
-
-            const keywords = extractKeywords(rewrittenArticle);
-
             // 1. Fetch the image URL for "AI" topic
-            const mediaData = await fetchMedia(keywords);
+            const mediaData = await fetchMedia( "technology", "crypto", "blockchain", "business", "innovation", "artificial intelligence", "robotics", "make money",);
             const filename = `ai_media_${Date.now()}`;
 
             let mediaId;
-
 
             if (mediaData.type === "image") {
                 const localImagePath = `./media/${filename}.jpg`;
@@ -188,7 +170,6 @@ const tweet = async () => {
                     media_ids: [mediaId]
                 }
             });
-
         } else {
             console.log("Failed to generate a tweet.");
         }
@@ -197,10 +178,11 @@ const tweet = async () => {
     }
 };
 
-// Schedule the tweet function to run every 10 minutes
-cron.schedule('*/1 * * * *', tweet);
+
+
+cron.schedule('0 * * * *', tweet);
 
 
 
-console.log("Started scheduler to tweet every 1 minutes.");
 
+console.log("Started scheduler to tweet once every hour");
